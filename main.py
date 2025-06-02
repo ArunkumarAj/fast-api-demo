be006578935477f52769d1f9cf27050d77cd7a41
ArunkumarAj <aurnroman123@gamil.com>
2025-06-02 16:00:01 +0530

    SMALL commit

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class Item(BaseModel):
+class Record(BaseModel):
     text: str = None
     is_done: bool = False
 
 
-items = []
+records = []
 
 
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    return {"message": "Hello, World"}
 
 
-@app.post("/items")
-def create_item(item: Item):
-    items.append(item)
-    return items
+@app.post("/records")
+def create_record(record: Record):
+    records.append(record)
+    return records
 
 
-@app.get("/items", response_model=list[Item])
-def list_items(limit: int = 10):
-    return items[0:limit]
+@app.get("/records", response_model=list[Record])
+def list_records(limit: int = 10):
+    return records[0:limit]
 
 
-@app.get("/items/{item_id}", response_model=Item)
-def get_item(item_id: int) -> Item:
-    if item_id < len(items):
-        return items[item_id]
+@app.get("/records/{record_id}", response_model=Record)
+def get_record(record_id: int) -> Record:
+    if record_id < len(records):
+        return records[record_id]
     else:
-        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+        raise HTTPException(status_code=404, detail=f"Record {record_id} not found")
