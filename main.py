53f771565141f1a18c26ea602b9554c76a8cced0
Arun <43464288+ArunkumarAj@users.noreply.github.com>
2025-06-02 16:00:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,30 +3,24 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-
 class Record(BaseModel):
     text: str = None
     is_done: bool = False
 
-
 records = []
-
 
 @app.get("/")
 def root():
     return {"message": "Hello, World"}
-
 
 @app.post("/records")
 def create_record(record: Record):
     records.append(record)
     return records
 
-
 @app.get("/records", response_model=list[Record])
 def list_records(limit: int = 10):
     return records[0:limit]
-
 
 @app.get("/records/{record_id}", response_model=Record)
 def get_record(record_id: int) -> Record:
